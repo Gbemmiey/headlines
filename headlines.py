dc64a556cc7699d9a84f0fa93b553702590a9551
Gbemmiey <gbemfemalabi@gmail.com>
2021-11-09 16:27:46 +0100

    Added jinja

--- headlines.py
+++ headlines.py
@@ -1,23 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import feedparser
 
 app = Flask(__name__)
-PUNCH_FEED = "https://rss.punchng.com/v1/category/latest_news"
 
+RSS_FEEDS = {
+    'bbc':'http://feeds.bbci.co.uk/news/world/africa/rss.xml',
+    'cnn': 'http://rss.cnn.com/rss/edition_africa.rss',
+    'punch': 'https://rss.punchng.com/v1/category/latest_news'
+    }
 
 @app.route('/')
 def getNews():
-    feed = feedparser.parse(PUNCH_FEED)
+    current_feed = RSS_FEEDS['bbc']
+    feed = feedparser.parse(current_feed)
     first_article = feed['entries'][0]
-    return """ <html>
-            <body>
-                <h1></h1>
-                <b>{0}</b> <br/>
-                <i>{1}</i> <br/>
-                <p>{2}</p> <br/>
-            </body>
-        </html>""".format(first_article.get("title"), first_article.get("published"),
-         first_article.get("summary"))
+    return render_template("home.html", article=first_article)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
